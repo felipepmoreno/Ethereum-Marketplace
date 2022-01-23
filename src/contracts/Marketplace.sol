401f2d488a775c6fdb17cd81250cf2c736bb01ea
felipepmoreno <felipepmoreno@gmail.com>
2022-01-23 19:26:59 -0300

    Product e teste criado

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -1,11 +1,27 @@
 pragma solidity >=0.4.21;
 
+//https://youtu.be/VH9Q2lf2mNo?t=2940
+
 contract Marketplace {
     string public name;
+    uint public productCount = 0;//we're using this to know how many products are in the blockchain
+    mapping(uint => Product) products; //key value relationship (hash). Declaring more than a single product
+
+
+    struct Product{
+        uint id; //uint = always +
+        string name;
+        uint price;
+        address owner;
+        bool purchased;
+    }
 
     constructor() public{
         name = "Marketplace";
     }
-}
 
-//https://youtu.be/VH9Q2lf2mNo?t=1830
+    function CreateProduct(string memory _name, uint _price) public{
+        productCount++;
+        products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+    }
+}
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -23,4 +23,17 @@ contract('Marketplace', (accounts) => {
             assert.equal(name, 'Marketplace')
         })
     })
+
+    describe('products', async() => {
+        let result, productCount
+
+        before(async()=>{
+            result = await marketplace.CreateProduct('Iphone X', web3.utils.toWei('1', 'ether'))
+            productCount = await marketplace.productCount()
+        })
+
+        it('creates products', async() => {
+            assert.equal(productCount, 1)
+        })
+    })  
 })
